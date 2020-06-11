25db67f18b7b4f818578c695b99259e1880e1fd2
Tomikland <tomikland@gmail.com>
2020-06-11 17:56:11 +0200

    Paging POC

--- SettingsPage.xaml.cs
+++ SettingsPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using KeyTrainWPF;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -9,20 +8,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
-using static Pythonic.ListHelpers;
 
 namespace KeyTrain
 {
     /// <summary>
-    /// Interaction logic for Window1.xaml
+    /// Interaction logic for SettingsPage.xaml
     /// </summary>
-    public partial class SettingsWindow : Window
+    public partial class SettingsPage : Page
     {
-        static ChainMap<string, dynamic> CFG = ConfigManager.Settings;
-        MainWindow owner;
-
-        public SettingsWindow()
+        public SettingsPage()
         {
             InitializeComponent();
         }
