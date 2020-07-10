2caaa4de12c2ad23099921b02cacd7ebf6359efd
Tomikland <tomikland@gmail.com>
2020-07-11 00:13:10 +0200

    Scatter plot

--- StatsPage.xaml.cs
+++ StatsPage.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Controls;
 using OxyPlot;
 using System.Linq;
 using OxyPlot.Axes;
+using OxyPlot.Series;
 
 namespace KeyTrain
 {
@@ -34,30 +35,43 @@ namespace KeyTrain
         {
             window = (MainWindow)Window.GetWindow(this);
             wpmplot.Title = "WPM";
+            var xaxis = wpmplot.Axes.ElementAt(0);
+            var yaxis = wpmplot.Axes.ElementAt(1);
+            yaxis.Title = "WPM";
+            xaxis.Title = "Sample";
             if(wpmlog.Count > 0)
             {
-                var yaxis = wpmplot.Axes.ElementAt(1);
+                xaxis.Maximum = wpmlog.Count;
                 yaxis.Minimum = wpmlog.Min();
                 yaxis.Maximum = wpmlog.Max();
-                yaxis.Title = "WPM";
-                var xaxis = wpmplot.Axes.ElementAt(0);
-                xaxis.Title = "Sample";
 
             }
+
+            wpmplot.InvalidatePlot(false);
         }
 
         private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         { 
 
             var smoothness = (int)e.NewValue;
-            var points = new List<DataPoint>();
+            var points = new List<ScatterPoint>();
             for (int i = smoothness; i < wpmlog.Count - smoothness; i++)
             {
-                points.Add(new DataPoint(i, wpmlog.Skip(i).Take(smoothness).Average()));
+                points.Add(new ScatterPoint(i,Math.Round( wpmlog.Skip(i).Take(smoothness).Average(), 2)));
             }
             
             wpmline.ItemsSource = points;
             wpmplot.InvalidatePlot(false);
+        }
+
+        void Refresh()
+        {
+            var smoothness = (int)wpmSmoothSlider.Value;
+            var points = new List<DataPoint>();
+            for (int i = smoothness; i < wpmlog.Count - smoothness; i++)
+            {
+                points.Add(new DataPoint(i, Math.Round(wpmlog.Skip(i).Take(smoothness).Average(), 2)));
+            }
         }
     }
 }
